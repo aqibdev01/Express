59baa4f7405898f53bd2bbce8994a197bd39c605
aqibabdullahdev@gmail.com <aqibabdullahdev@gmail.com>
2025-07-29 22:50:24 +0500

    ejs

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-import express from 'express'
-import blog from "./routes/blog.js"
-const app = express()
-const port = 3000
+import express from "express";
+import blog from "./routes/blog.js";
+const app = express();
+const port = 3000;
 
-app.use(express.static("public"))
-app.use('/blog', blog)
+app.use(express.static("public"));
+app.use("/blog", blog);
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hi This is a get request!')
-})
-app.post('/', (req, res) => {
-  res.send('Post Request')
-})
+app.get("/", (req, res) => {
+  let siteName = "Trail Express";
+  let search = "Search Here";
+  res.render("index", { siteName: siteName, search: search });
+});
+
+app.post("/", (req, res) => {
+  res.send("Post Request");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
